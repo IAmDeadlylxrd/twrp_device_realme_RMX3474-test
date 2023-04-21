a07cbabd9a53fbfa1418e9f1ea7169058e2924a8
IAmDeadlylxrd <contact@deadlylxrd.me>
2023-04-21 21:44:59 +0500

    Initial test TWRP tree for RMX3474

---
+++ device.mk
@@ -0,0 +1,82 @@
+#
+# Copyright (C) 2023 Team Win Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
+# Apex libraries
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so \
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libcuuc.so
+
+# Bootctrl
+PRODUCT_PACKAGES += \
+    bootctrl.holi.recovery \
+    android.hardware.boot@1.1-impl-qti.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# HIDL
+PRODUCT_ENFORCE_VINTF_MANIFEST := true
+
+# Recovery device modules
+TARGET_RECOVERY_DEVICE_MODULES += \
+    android.hidl.base@1.0 \
+    libandroidicu \
+    libcap \
+    libdrm \
+    libion \
+    libpcrecpp \
+    libxml2
+
+# Recovery library source files
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.base@1.0.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libcap.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libdrm.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpcrecpp.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+
+# Shipping API Level
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
+# Qualcomm Decryption
+PRODUCT_PACKAGES_ENG += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+# VNDK Version
+PRODUCT_TARGET_VNDK_VERSION := 31
---
+++ twrp_RMX3474.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2023 Team Win Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit device configuration
+$(call inherit-product, device/realme/RMX3474/device.mk)
+
+# Inherit some common TWRP stuff
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Configure base.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Configure core_64_bit_only.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Configure gsi_keys.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Configure virtual_ab_ota.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Configure emnulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Device assert
+TARGET_OTA_ASSERT_DEVICE := RMX3474
+
+# Release name
+PRODUCT_RELEASE_NAME := RMX3474
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := RMX3474
+PRODUCT_NAME := twrp_RMX3474
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := 9 5G
+PRODUCT_MANUFACTURER := realme
